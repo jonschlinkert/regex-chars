c3879f75734d26b81d16ba8fbda93632297bbc0a
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 05:01:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * regex-chars <https://github.com/jonschlinkert/regex-chars>
+ *
+ * Copyright (c) 2015 Jon Schlinkert.
+ * Licensed under the MIT license.
+ */
+
+module.exports = '.*+?^${}()|[\\]\\/\\\\';
